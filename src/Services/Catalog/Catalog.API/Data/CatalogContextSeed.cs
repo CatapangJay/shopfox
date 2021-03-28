b9320d670505e3ef08a3993b3573502bc17b4026
CatapangJay <38466188+CatapangJay@users.noreply.github.com>
2021-03-28 21:47:29 +0800

    Added catalog service

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public static class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool productExists = productCollection.Find(p => true).Any();
+
+            if (!productExists)
+            {
+                productCollection.InsertManyAsync(GetProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetProducts()
+        {
+            var productFaker = new Faker<Product>()
+                .RuleFor(o => o.Id, f => Guid.NewGuid().ToString())
+                .RuleFor(o => o.Name, f => f.Commerce.Product())
+                .RuleFor(o => o.Category, f => f.Commerce.Categories(new Random().Next())[new Random().Next()])
+                .RuleFor(o => o.Summary, f => f.Commerce.ProductAdjective())
+                .RuleFor(o => o.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(o => o.Description, f => f.Image.LoremPixelUrl("business"))
+                .RuleFor(o => o.Price, f => Decimal.Parse(f.Commerce.Price()));
+
+            return productFaker.Generate(20);
+        }
+    }
+}
